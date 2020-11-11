696b95200d6820657e7bd461682d173a7c45c891
Dmitrii <ds.shklyaev@gmail.com>
2020-11-11 23:59:17 +0500

    task1_cUpdate1

--- DB_MySQL_hw3_task1_Shkliaev.sql
+++ DB_MySQL_hw3_task1_Shkliaev.sql
@@ -3,4 +3,11 @@
 1. Сделать запрос, в котором мы выберем все данные о городе - регион, страна.
 2. Выбрать все города из Московской области.
 */
-в работе
+SELECT 
+    _countries.title, _regions.title, _cities.title
+FROM
+    geodata._cities
+        LEFT JOIN
+    geodata._regions ON _cities.region_id = _regions.id
+        LEFT JOIN
+    geodata._countries ON _regions.country_id = _countries.id;
