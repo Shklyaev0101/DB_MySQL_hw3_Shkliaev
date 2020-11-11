2b2b660e7f8e4fba0023991f6a030c674530c1bc
Dmitrii <ds.shklyaev@gmail.com>
2020-11-12 00:24:10 +0500

    task1_cUpdate2

--- DB_MySQL_hw3_task1_Shkliaev.sql
+++ DB_MySQL_hw3_task1_Shkliaev.sql
@@ -1,7 +1,6 @@
 /*Task 1
 База данных "Страны и города мира":
 1. Сделать запрос, в котором мы выберем все данные о городе - регион, страна.
-2. Выбрать все города из Московской области.
 */
 SELECT 
     _countries.title, _regions.title, _cities.title
@@ -11,3 +10,15 @@ FROM
     geodata._regions ON _cities.region_id = _regions.id
         LEFT JOIN
     geodata._countries ON _regions.country_id = _countries.id;
+
+/*
+2. Выбрать все города из Московской области.
+*/
+SELECT 
+    _regions.title, _cities.title
+FROM
+    geodata._cities
+        LEFT JOIN
+    _regions ON _cities.region_id = _regions.id
+WHERE
+    _regions.id = 1053480;
